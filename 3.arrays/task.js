dc1d9fb3db2d97bd6113c445d23661c9e0dff239
komshuk <komshuk@yandex.ru>
2023-06-21 22:23:38 +0300

    Initian commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,7 @@
 function compareArrays(arr1, arr2) {
-  
+    return  arr1.length === arr2.length && arr1.every((number, index) => number === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    return ((users.filter(user => user.gender === gender)).map(user => user.age).reduce((acc, item, index, users) => acc + item/users.length, 0));
 }
