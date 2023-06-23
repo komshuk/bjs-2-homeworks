202ee0fa8570bc14cee42ae327ed5bcad124f01f
komshuk <komshuk@yandex.ru>
2023-06-23 22:20:55 +0300

    Initian commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,32 @@
-function Student(name, gender, age) {
+function Student(name, gender, age, marks) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
+  }
   
-}
-
-Student.prototype.setSubject = function (subjectName) {
+  Student.prototype.setSubject = function (subjectName) {
+    this.subject = subjectName;
+  }
   
-}
-
-Student.prototype.addMarks = function (...marks) {
+  Student.prototype.addMarks = function (...marks) {
+    if (!this.marks){
+      return;
+    }  
+    this.marks.push(...marks);
+  }
   
-}
-
-Student.prototype.getAverage = function () {
+  Student.prototype.getAverage = function () {
+      if (!this.marks || this.marks.length === 0){
+          return 0;
+      }
+          const sum = this.marks.reduce((acc, mark) => acc + mark, 0);
+          const length = this.marks.length;
+          return sum / length;
+  }
   
-}
-
-Student.prototype.exclude = function (reason) {
-  
-}
+  Student.prototype.exclude = function (reason) {
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
+  }
