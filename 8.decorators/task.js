e8f623b5842d94d5c88cc07e671bdd69ca775069
komshuk <komshuk@yandex.ru>
2023-07-11 22:03:20 +0300

    Initian commit

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,46 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+  let cache = [];
+
+  function wrapper(...args) {
+    const hash = md5(JSON.stringify(args));
+    let objectInCache = cache.find((item) => item.hash === hash);
+
+    if (objectInCache) {
+      console.log("Из кэша: " + objectInCache.value);
+      return "Из кэша: " + objectInCache.value;
+    }
+
+    let result = func(...args);
+    cache.push({ hash, value: result });
+
+    if (cache.length > 5) {
+      cache.shift();
+    }
+
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  }
+  return wrapper;
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+  let timeoutId = null;
+  function wrapper(...args) {
+    clearTimeout(timeoutId);
+    if (!timeoutId) {
+      func(...args);
+      wrapper.count++;
+    }
+    timeoutId = setTimeout(() => {
+      func(...args);
+      wrapper.count++;
+    }, delay);
+    wrapper.allCount++;
+  }
+
+  wrapper.count = 0;
+  wrapper.allCount = 0;
+  return wrapper;
 }
